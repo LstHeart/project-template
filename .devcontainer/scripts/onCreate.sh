820668ae368ca4c5bac101c24ab1bb34c13f8dc4
LstHeart <dennis.lstheart@qq.com>
2022-07-29 09:28:25 +0000

    refactor: adjust script

--- .devcontainer/scripts/onCreate.sh
+++ .devcontainer/scripts/onCreate.sh
@@ -7,5 +7,23 @@ echo `date +"[%Y/%m/%d %H:%M:%S]"` 'onCreateCommand will begin✨' \
 # install global npms
 npm install -g @devcontainers/cli @antfu/ni vsce yo
 
+## setup fish
+if [ ! -d ~/.config/fish/conf.d ]; then
+  mkdir -p ~/.config/fish/conf.d
+fi
+curl https://raw.githubusercontent.com/skywind3000/z.lua/master/z.lua > ~/.config/fish/conf.d/z.lua \
+&& echo "lua ~/.config/fish/conf.d/z.lua --init fish | source" > ~/.config/fish/conf.d/z.fish
+
+fish -c "fisher install jorgebucaran/nvm.fish"
+fish -c "fisher install PatrickF1/fzf.fish"
+fish -c "fisher install franciscolourenco/done "
+fish -c "fisher install jorgebucaran/autopair.fish"
+fish -c "fisher install andreiborisov/sponge"
+fish -c "echo y | fisher install ilancosman/tide@v5"
+
+# git-completion
+wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash \
+  -O $HOME/.git-completion.bash
+
 echo `date +"[%Y/%m/%d %H:%M:%S]"` 'onCreateCommand has done🎉'
 exit
--- .devcontainer/scripts/postCreate.sh
+++ .devcontainer/scripts/postCreate.sh
@@ -7,22 +7,22 @@ echo `date +"[%Y/%m/%d %H:%M:%S]"` 'postCreateCommand will begin✨' \
 # ln -sf $HOME/config $PWD/.devcontainer/config && set +x
 
 ## setup fish
-if [ ! -d ~/.config/fish/conf.d ]; then
-  mkdir -p ~/.config/fish/conf.d
-fi
-curl https://raw.githubusercontent.com/skywind3000/z.lua/master/z.lua > ~/.config/fish/conf.d/z.lua \
-&& echo "lua ~/.config/fish/conf.d/z.lua --init fish | source" > ~/.config/fish/conf.d/z.fish
+# if [ ! -d ~/.config/fish/conf.d ]; then
+#   mkdir -p ~/.config/fish/conf.d
+# fi
+# curl https://raw.githubusercontent.com/skywind3000/z.lua/master/z.lua > ~/.config/fish/conf.d/z.lua \
+# && echo "lua ~/.config/fish/conf.d/z.lua --init fish | source" > ~/.config/fish/conf.d/z.fish
 
-fish -c "fisher install jorgebucaran/nvm.fish"
-fish -c "fisher install PatrickF1/fzf.fish"
-fish -c "fisher install franciscolourenco/done "
-fish -c "fisher install jorgebucaran/autopair.fish"
-fish -c "fisher install andreiborisov/sponge"
-fish -c "echo y | fisher install ilancosman/tide@v5"
+# fish -c "fisher install jorgebucaran/nvm.fish"
+# fish -c "fisher install PatrickF1/fzf.fish"
+# fish -c "fisher install franciscolourenco/done "
+# fish -c "fisher install jorgebucaran/autopair.fish"
+# fish -c "fisher install andreiborisov/sponge"
+# fish -c "echo y | fisher install ilancosman/tide@v5"
 
-# git-completion
-wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash \
-  -O $HOME/.git-completion.bash
+# # git-completion
+# wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash \
+#   -O $HOME/.git-completion.bash
 
 echo `date +"[%Y/%m/%d %H:%M:%S]"` 'postCreateCommand has done🎉'
 exit
