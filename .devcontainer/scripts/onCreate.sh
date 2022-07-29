a6ca31ae55a2fa3a93646131f4ed856da6044079
LstHeart <dennis.lstheart@qq.com>
2022-07-29 09:10:44 +0000

    chore: update dockerfile

--- .devcontainer/scripts/onCreate.sh
+++ .devcontainer/scripts/onCreate.sh
@@ -4,23 +4,8 @@
 echo `date +"[%Y/%m/%d %H:%M:%S]"` 'onCreateCommand will begin✨' \
   && echo '[working-dir]:' `pwd`
 
-# setup nvm
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install 14 && nvm install 16 \
-  && nvm default 14
 # install global npms
-npm install -g @devcontainers/cli @antfu/ni pnpm yarn vsce yo
-
-## install fzf
-brew install fzf && echo y y y| $(brew --prefix)/opt/fzf/install
-
-# install other softs by homebrew
-brew install fisher \
-    lua luarocks \
-    gh git-lfs \
-    ranger
+npm install -g @devcontainers/cli @antfu/ni vsce yo
 
 echo `date +"[%Y/%m/%d %H:%M:%S]"` 'onCreateCommand has done🎉'
 exit
