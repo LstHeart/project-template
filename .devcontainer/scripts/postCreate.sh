d85ab2fd9d9afee3086b20eae0499e2b00cf65b3
LstHeart <dennis.lstheart@qq.com>
2022-07-30 14:41:50 +0000

    update

--- .devcontainer/scripts/postCreate.sh
+++ .devcontainer/scripts/postCreate.sh
@@ -9,7 +9,7 @@ echo `date +"[%Y/%m/%d %H:%M:%S]"` 'postCreateCommand will begin✨' \
 # 重构时要保留的数据
 ln -sf $PWD/.devcontainer/config/.zlua $HOME/.zlua && set +x
 ln -sf $PWD/.devcontainer/config/fish_history $HOME/.local/share/fish/fish_history && set +x
-ln -sf $PWD/.devcontainer/config/.bash_history $HOME/.bash_history && set +x
+ln -sf $PWD/.devcontainer/config/.bash_history $HOME/.bash_history
 ln -sf $PWD/.devcontainer/config/.nirc $HOME/.nirc && set +x
 
 echo `date +"[%Y/%m/%d %H:%M:%S]"` 'postCreateCommand has done🎉'
